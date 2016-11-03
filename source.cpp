4cecdae4c9698644418afc7802b64c1243cd4f97
AndyBay <AndyBay@users.noreply.github.com>
2016-11-03 04:53:45 +0100

    Source file

---
+++ source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+bool samePrimeFactor(unsigned long long m, unsigned long long c);
+
+int main() {
+	unsigned long long 	i = 1, 
+						mersenne,
+						candidate;
+						
+	std::cout << "\nFinds candidates using <<mersanne>> and checks if they"
+			  << " are valid.\nProceeds to print out the <<perfect numbers>>"
+			  << " until the amount of \ndigits overreaches its variable.\n";
+						
+	do {
+		i++;
+		mersenne = (pow(2, i) - 1);
+		candidate = (pow(2, i-1)) * mersenne;
+		
+		if (samePrimeFactor(mersenne, candidate)){
+			std::cout << "\nPerfect number: " << candidate;
+			if (mersenne < 999) std::cout << '\t';
+			if (mersenne < 999999) std::cout << '\t';
+			std::cout << "\twith mersenne: " << mersenne;
+		}
+		
+	} while (candidate != 0);
+	
+	return 0;
+}
+
+bool samePrimeFactor(unsigned long long m, unsigned long long c) {
+	
+	unsigned long long i = 2;
+
+	while (i*i <= c) {
+		if (c % i == 0) 
+			c /= i;
+		else
+			i++;
+	}
+	if (c == m)
+		return 1;
+	
+	return 0;
+}
